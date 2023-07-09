a8760ccb4ce2b8dbae32c325e19ddfc0581452e7
dmb-tech <65574932+dmb-tech@users.noreply.github.com>
2023-07-09 12:45:16 -0400

    google login

--- src/components/Register.js
+++ src/components/Register.js
@@ -24,7 +24,7 @@ function Register() {
         localStorage.setItem("token", res.data.token);
       })
       .catch((err, res) => {
-        console.log(err);
+        console.log(err.response);
       });
   }
 
